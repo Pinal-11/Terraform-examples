3c1db1e6d95505a6da67c5b621dbc815392a3150
Pinal-11 <dadhania.pinal@gmail.com>
2021-08-17 13:54:11 +0530

    VPC

--- vpc.tf
+++ vpc.tf
@@ -23,7 +23,6 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id                          = aws_vpc.vpc.id
   cidr_block                      = var.public-subnet-1-cidr
   availability_zone               = "ap-south-1a"
-  map_customer_owned_ip_on_launch = true
 
   tags = {
     Name = "Public subnet 1"
@@ -55,4 +54,56 @@ resource "aws_route_table" "public-route-table" {
     Name = "Public route table"
   }
 
+}
+
+# Associate subnet with Route table
+resource "aws_route_table_association" "route-table-association" {
+  subnet_id = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+#create a network interface
+resource "aws_network_interface" "network-interface" {
+  subnet_id = aws_subnet.public-subnet-1.id
+  private_ips = ["10.0.0.50"]
+  security_groups = [aws_security_group.ssh-security-group.id]
+}
+
+#create an elastic IP to the network interface 
+resource "aws_eip" "eip" {
+  vpc = true
+  network_interface = aws_network_interface.network-interface.id
+  associate_with_private_ip = "10.0.0.50"
+  depends_on = [
+    aws_internet_gateway.internet-gateway
+  ]
+}
+
+#create ubuntu server an install apache2
+resource "aws_instance" "web" {
+  ami = "ami-04db49c0fb2215364"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1a"
+  tag = {
+    Name = "Ubuntu"
+  }
+  key_name = "Pinal25"
+
+  network_interface {
+   device_index = 0
+   network_interface_id = aws_network_interface.network-interface.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c "echo Your Frist Web Server" > /var/www/html/index.html
+              sudo chkconfig on
+              EOF
+  
+  tags={
+    Name = "Web Server"
+  }
 }
